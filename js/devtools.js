a81965b6f46e5b95488e6a33a5e4fef68a09f521
move132 <move11@126.com>
2016-08-12 18:38:32 +0800

    update

---
+++ js/devtools.js
@@ -0,0 +1,10 @@
+chrome.devtools.panels.create("Mys Panel",   "../images/appicon.jpg",
+	"../page/options1.html",   function(panel) {
+		cosnole.log(panel);
+      // code invoked on panel creation
+
+    }
+);
+
+
+
