45741fa7afb36e3b05664e618b8bc50468191801
move132 <move11@126.com>
2016-12-09 22:31:04 +0800

    update file

--- js/console.js
+++ js/console.js
@@ -55,8 +55,10 @@ $(function(){
 	});
 
 
-	if (window.location.hostname === "egghead.io") {
-		$(".lesson-controls-container").append('<a id="a4883534254543aeb0" target="_blank" href='+$("#wistia_19_source").attr("src")+' title="" class="custom-btn" data-original-title="下载视频"><i class="fa fa-download"></i> <span class="hidden-xs">下载视频</span></a>');
-	}; 
+	$(function(){
+		if (window.location.hostname === "egghead.io") {
+			$(".lesson-controls-container").append('<a id="a4883534254543aeb0" target="_blank" href='+$("#wistia_19_source").attr("src")+' title="" class="custom-btn" data-original-title="下载视频"><i class="fa fa-download"></i> <span class="hidden-xs">下载视频</span></a>');
+		}; 
+	})
 });
 
