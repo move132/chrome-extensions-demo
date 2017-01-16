3bf61ffd82190d30fb8b3da53a76a4e0feb7f109
move132 <move11@126.com>
2017-01-16 09:31:49 +0800

    新增数据抓取

--- js/common.js
+++ js/common.js
@@ -27,20 +27,43 @@
 function callback(notificationsID){
   	console.log('uppop done!'+notificationsID);
 }*/
- 
+
 /*fetch('http://apis.baidu.com/songshuxiansheng/news/news',{
 	headers: {
 		"apikey": "90686ef7dd90bc83f9aa9830f7bfdbeb",
 	},
 }).then(function(response){return response.text();}).then(function(responseText){
-	setBodyHtml(responseText); 
+	setBodyHtml(responseText);
 }).catch(function(error){
 	console.log(error);
 });*/
 
-ajax('http://apis.baidu.com/songshuxiansheng/news/news',function(responseText){
-	setBodyHtml(responseText); 
+/*ajax('http://apis.baidu.com/songshuxiansheng/news/news',function(responseText){
+	 setBodyHtml(responseText);
+});*/
+
+fetch('http://www.biuss.com/').then(function(response){
+	return response.text();
+}).then(function(responseText){
+    console.log($(responseText).find(".mega-menu .btn-u").html());
+    $(".list").html( $(responseText).find(".mega-menu .btn-u").html().replace(/\[每1小时变更一次密码\]/g,''));
+
+    fetch('http://www.biuss.com/ucenter/free_pwd.php').then(function(response){
+    	return response.text();
+    }).then(function(responseText){
+        console.log(responseText);
+        $("#free_pwd").html( responseText.split('=')[1].replace(/\'/g,'').replace(/;/g,'') );
+    }).catch(function(error){
+        console.log(error);
+    });
+
+}).catch(function(error){
+    console.log(error);
 });
+
+
+
+
 
 /*for (var i = 0; i < 20; i++) {
 	ajax('http://apis.baidu.com/txapi/mvtp/meinv?num=50',function(responseText){
@@ -48,14 +71,14 @@ ajax('http://apis.baidu.com/songshuxiansheng/news/news',function(responseText){
 		var str='';
 		for (var i = 0; i < data.length; i++) {
 			str+='<img width="90" height="90" src="'+data[i].picUrl+'" alt="" />';
-		}; 
+		};
 		$(".box:eq(2)").append(str);
 	});
 }*/
 
 function ximiPromise(i){
 	return new Promise((resolve, reject) => {
-		ajax('http://m.ximalaya.com/album/more_tracks?url=%2Falbum%2Fmore_tracks&aid=203355&page='+(i+1),function(responseText){ 
+		ajax('http://m.ximalaya.com/album/more_tracks?url=%2Falbum%2Fmore_tracks&aid=203355&page='+(i+1),function(responseText){
 			$(".box:eq(2)").append(JSON.parse(responseText).html).find("a").removeAttr("href");
 			resolve(JSON.parse(responseText));
 		});
@@ -78,7 +101,7 @@ ximiPromise(0).then(function(res){
 
 /*var xiamilist=[];
 for (var i = 0; i < 10; i++) {
-	xiamilist.push(ximiPromise(i)); 
+	xiamilist.push(ximiPromise(i));
 };
 
 Promise.all(xiamilist).then(function(res){
@@ -90,8 +113,8 @@ Promise.all(xiamilist).then(function(res){
 
 
 $(function(){
-	$(".menu-box ul li").on("click",function(e){ 
-		var _index=$(this).index(); 
+	$(".menu-box ul li").on("click",function(e){
+		var _index=$(this).index();
 		$(".menu-box ul li").removeClass("active").eq(_index).addClass("active");
 		$(".box").hide().eq(_index).show();
 	});
@@ -99,8 +122,8 @@ $(function(){
 	$(".getMsg").on("click",function(e){
 		var _this=this;
 		fetch('http://apis.baidu.com/appangel/shenzhentong/shenzhentong?id=322170397&format=json',{
-			headers: { 
-				"apikey": "90686ef7dd90bc83f9aa9830f7bfdbeb", 
+			headers: {
+				"apikey": "90686ef7dd90bc83f9aa9830f7bfdbeb",
 			},
 		}).then(function(response){return response.text();}).then(function(responseText){
 			var data=JSON.parse(responseText).data;
@@ -117,23 +140,23 @@ $(function(){
 	$(".more").on("click",function(e){
 		$("body").addClass("loading_body");
 		ajax('http://apis.baidu.com/songshuxiansheng/news/news',function(responseText){
-			setBodyHtml(responseText); 
+			setBodyHtml(responseText);
 			setTimeout(function(){$("body").removeClass("loading_body");}, 300);
 		});
 	});
 
 	$("body").append("<audio id='jp_audio_0' preload='metadata'  ></audio>");
-	$("body").on("click",".btn-player",function(){   
+	$("body").on("click",".btn-player",function(){
 		//alert($(this).attr("sound_url"));
 		chrome.windows.create({
             url:$(this).attr("sound_url"),
            // tabId:122221,
             width:300,
-            height:300, 
+            height:300,
             type:"panel"
         });
-	 
-		 
+
+
 		$("#jp_audio_0").attr("src",$(this).attr("sound_url"));
 	});
 });
@@ -142,7 +165,7 @@ $(function(){
 
 
 function setBodyHtml(responseText){
-	var data=JSON.parse(responseText),str='<ul class="article_list">'; 
+	var data=JSON.parse(responseText),str='<ul class="article_list">';
 	if(data.retData.length >0){
 		for(var i=0 ; i < data.retData.length; i++){
 			str+='<li>' +
@@ -164,8 +187,8 @@ function setBodyHtml(responseText){
 
 function ajax(url,callback){
 	fetch(url,{
-		headers: { 
-			"apikey": "90686ef7dd90bc83f9aa9830f7bfdbeb", 
+		headers: {
+			"apikey": "90686ef7dd90bc83f9aa9830f7bfdbeb",
 		},
 	}).then(function(response){return response.text();}).then(function(responseText){
 		 callback && callback(responseText);
@@ -204,13 +227,3 @@ document.querySelector(".clearMessage").addEventListener("click",function(e){
 	if(interFn){clearInterval(interFn);}
 	console.log('清除定时器!!!');
 });*/
-
- 
-
-
-
-
-
-
- 
-
